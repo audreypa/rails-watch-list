717358af6c3ce26d83a4e827d0a7b0c495d4b73c
Audrey Pachol <audrey.pachol@gmail.com>
2023-06-15 11:48:56 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :lists, except: %i[destroy edit] do
+    resources :bookmarks, except: %i[update create]
+  end
+  resources :bookmarks, only: :destroy
   # Defines the root path route ("/")
-  # root "articles#index"
 end
