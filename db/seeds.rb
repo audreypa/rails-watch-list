be7357cf581a5f718cf34ba0918f7e76de697637
Audrey Pachol <audrey.pachol@gmail.com>
2023-06-16 17:00:02 +0200

    css

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,20 +3,20 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+  movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+  Character.create(name: "Luke", movie: movies.first)
 
-# Movie.destroy_all
-# url = 'http://tmdb.lewagon.com/movie/top_rated?api_key=<your_api_key>'
-# movies = JSON.parse(URI.open(url).read)['results']
-# movies.each do |movie|
-#   Movie.create!(
-#     title: movie['title'],
-#     overview: movie['overview'],
-#     poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
-#     rating: movie['vote_average']
-#   )
-# end
+Movie.destroy_all
+url = 'http://tmdb.lewagon.com/movie/top_rated?api_key=<your_api_key>'
+movies = JSON.parse(URI.open(url).read)['results']
+movies.each do |movie|
+  Movie.create!(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
+    rating: movie['vote_average']
+  )
+end
 
 Bookmark.destroy_all
 List.destroy_all
